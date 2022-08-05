9c25c5209d152e8462474b80bacb105eb03d2566
Skota11 <91359399+Skota11@users.noreply.github.com>
2022-08-05 13:04:43 +0900

    app video_quality option

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function save(id, res) {
   const url = BASE_PATH + youtubeId;
   const moviepath = path.join(__dirname, '/' + youtubeId + '_video.mp4')
   const audiopath = path.join(__dirname, `/${youtubeId}_audio.wav`)
-  const mixvideopath = path.join(__dirname, '/' + youtubeId + '.mp4')
+  const mixvideopath = path.join(__dirname, '/' + youtubeId + '_highest.mp4')
 
-  const audio_ytdl = ytdl(url , {quality:'highestaudio'})
+  const audio_ytdl = ytdl(url, { quality: 'highestaudio' })
   audio_ytdl.pipe(fs.createWriteStream(`${youtubeId}_audio.wav`));
 
   const video_ytdl = ytdl(url, { quality: 'highestvideo' })
   video_ytdl.pipe(fs.createWriteStream(`${youtubeId}_video.mp4`));
-  
+
   video_ytdl.on('end', () => {
     ffmpeg()
       .addInput(moviepath)
@@ -27,10 +27,10 @@ function save(id, res) {
       .output(mixvideopath)
       .outputOptions(['-map 0:v', '-map 1:a', '-c:a aac', '-c:v copy'])
       .on('start', (command) => {
-//         console.log('TCL: command -> command', command)
+        console.log('TCL: command -> command', command)
       })
       .on('error', (error) => console.log("errrrr", error))
-      .on('end', () => { console.log("Conmpale"); res.send(`<p>大変長らくお待たせしました。 => </p><a href='https://ytdl.skota11.repl.co/v/${youtubeId}'>動画</a>`); })
+      .on('end', () => { console.log("Conmpale"); res.send(`<p>MovieLink</p><a href='https://ytdl.skota11.repl.co/v/${youtubeId}/highest'>Movie</a>`); })
       .run()
   });
 }
@@ -38,22 +38,35 @@ function save(id, res) {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/home.html'))
 });
-
 app.get('/v/:id', (req, res) => {
-  v_path = path.join(__dirname, '/' + req.params.id + '.mp4');
+  res.send("Please enter the movie quality")
+});
+app.get('/v/:id/:rank', (req, res) => {
+  v_path = path.join(__dirname, '/' + req.params.id + '_' + req.params.rank +'.mp4');
   if (fs.existsSync(v_path)) {
     res.sendFile(v_path);
   } else {
-    res.send("NotFoundVideo");
+    res.send("Sorry, We couldn't find the movie");
   }
 });
 
-app.get('/s/:id', (req, res) => {
+app.get('/s/:id/highest', (req, res) => {
   const Id = req.params.id;
-  if(ytdl.validateID(Id)){
-   save(Id, res); 
-  }else{
-    res.send("NotId")
+  if (ytdl.validateID(Id)) {
+    save(Id, res);
+  } else {
+    res.send("Sorry, This is not youtubeID")
+  }
+});
+app.get('/s/:id/normal', (req, res) => {
+  const youtubeId = req.params.id;
+  if (ytdl.validateID(youtubeId)) {
+    const url = BASE_PATH + youtubeId;
+    const video_ytdl = ytdl(url, { quality: 'highest' })
+    video_ytdl.pipe(fs.createWriteStream(`${youtubeId}_normal.mp4`));
+    res.send(`<p>MovieLink</p><a  href='https://ytdl.skota11.repl.co/v/${youtubeId}/normal'>Movie</a>`);
+  } else {
+    res.send("Sorry, This is not youtubeID")
   }
 });
 
