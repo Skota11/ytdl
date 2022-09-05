3afc5c175acf09916e390db9e82a735178a5c457
S Kota <91359399+Skota11@users.noreply.github.com>
2022-09-05 08:18:23 +0000

    add LICENCE

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var path = require("path");
+const path = require("path");
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
@@ -30,31 +30,53 @@ function save_highest(id, res) {
         console.log('TCL: command -> command', command)
       })
       .on('error', (error) => console.log("errrrr", error))
-      .on('end', () => { console.log("Conmpale"); res.send(`<p>MovieLink</p><a href='https://ytdl.skota11.repl.co/v/${youtubeId}/highest'>Movie</a>`); })
+      .on('end', () => {
+        console.log("Conmpale");
+        res.redirect(`/v/${youtubeId}/highest`);
+        delate(youtubeId , 1);
+      })
       .run()
   });
 }
+function sleep(waitSec) {
+  return new Promise(function(resolve) {
+    setTimeout(function() { resolve() }, waitSec);
+  });
+}
+async function delate(id, mode) {
+  await sleep(86400000)
+  console.log(`Delate,${id}`)
+  if (mode == 1) {
+    fs.unlinkSync(path.join(__dirname + `/${id}_video.mp4`))
+    fs.unlinkSync(path.join(__dirname + `/${id}_audio.wav`))
+    fs.unlinkSync(path.join(__dirname + `/${id}_highest.mp4`))
+  } else if (mode == 2) {
+    fs.unlinkSync(path.join(__dirname + `/${id}_normal.mp4`))
+  } else if (mode == 3) {
+    fs.unlinkSync(path.join(__dirname + `/${id}_audio.wav`))
+  }
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/home.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 app.get('/v/:id', (req, res) => {
-  res.send("Please enter the movie quality")
+  res.send("Enter the video quality")
 });
 app.get('/v/:id/:rank', (req, res) => {
   v_path = path.join(__dirname, '/' + req.params.id + '_' + req.params.rank + '.mp4');
   if (fs.existsSync(v_path)) {
     res.sendFile(v_path);
   } else {
-    res.send("Sorry, We couldn't find the movie");
+    res.send("We couldn't find the video");
   }
 });
 app.get('/a/:id', (req, res) => {
-    a_path = path.join(__dirname, '/' + req.params.id + '_audio.wav');
+  a_path = path.join(__dirname, '/' + req.params.id + '_audio.wav');
   if (fs.existsSync(a_path)) {
     res.sendFile(a_path);
   } else {
-    res.send("Sorry, We couldn't find the audio");
+    res.send("We couldn't find the audio");
   }
 })
 
@@ -63,7 +85,7 @@ app.get('/s/:id/highest', (req, res) => {
   if (ytdl.validateID(Id)) {
     save_highest(Id, res);
   } else {
-    res.send("Sorry, This is not youtubeID")
+    res.send("NotYouTubeID")
   }
 });
 app.get('/s/:id/normal', (req, res) => {
@@ -72,18 +94,28 @@ app.get('/s/:id/normal', (req, res) => {
     const url = BASE_PATH + youtubeId;
     const video_ytdl = ytdl(url, { quality: 'highest' })
     video_ytdl.pipe(fs.createWriteStream(`${youtubeId}_normal.mp4`));
-    res.send(`<p>MovieLink</p><a  href='https://ytdl.skota11.repl.co/v/${youtubeId}/normal'>Movie</a>`);
+    video_ytdl.on('end' , () => {
+          res.redirect(`/v/${youtubeId}/normal`);
+    delate(youtubeId , 2);
+    })
   } else {
-    res.send("Sorry, This is not youtubeID")
+    res.send("NotYouTubeID")
   }
 });
 app.get('/s/:id/audio', (req, res) => {
   const youtubeId = req.params.id;
+  if (ytdl.validateID(youtubeId)) {
   const url = BASE_PATH + youtubeId;
   const audio_ytdl = ytdl(url, { quality: 'highestaudio' })
   audio_ytdl.pipe(fs.createWriteStream(`${youtubeId}_audio.wav`));
 
-  res.send(`<p>AudioLink</p><a href='https://ytdl.skota11.repl.co/a/${youtubeId}'>Audio</a>`)
+  audio_ytdl.on('end', () => {
+    res.redirect(`/a/${youtubeId}`);
+    delate(youtubeId , 3);
+  })
+  }else {
+    res.send("NotYouTubeID")
+  }
 })
 
 app.listen(3000, () => {
